497502e2431f1945bccdaaf9f5bf9821b4924bdc
Seraphim <s.k.ivanov314@gmail.com>
2022-04-07 17:04:32 +0300

    "v1.2"

--- TOE_lab7/Calculus.cpp
+++ TOE_lab7/Calculus.cpp
@@ -1,4 +1,15 @@
 #include "Calculus.h"
+#include <algorithm>
+
+
+
+bool comp(std::vector<double> left, std::vector<double> right)
+{
+
+	return left[0] > right[0];
+
+}
+
 Calculus::Calculus(double _I, double _U0, double _f0, double _Ic0, std::vector<std::vector<double>> _table, bool acc)
 {
 	I[0] = _I * pow(10, -3);
@@ -19,10 +30,15 @@ Calculus::Calculus(double _I, double _U0, double _f0, double _Ic0, std::vector<s
 	f0[0] = _f0 * pow(10, 3);
 	f0[1] = w0_accuracy;
 
+	// let's calculate it
 	get_GR();
 	get_Q_rho();
 	get_LC();
 	get_vec();
+
+	// resault
+	out();
+	out_vec();
 }
 double Calculus::multi_accuracy(std::vector<double*> units)
 {
@@ -39,22 +55,14 @@ void Calculus::get_GR()
 	G[1] = G[0] * multi_accuracy({ I, U0 });
 	R[0] = U0[0] / I[0];
 	R[1] = R[0] * multi_accuracy({ I, U0 });
-
-
-	std::cout.precision(3);
-	std::cout << "G = (" << G[0] * pow(10, 6) << " +- " << G[1] * pow(10, 6) << ") \\nuSm\n";
-	std::cout.precision(3);
-	std::cout << "R = (" << R[0] * pow(10, -3) << " +- " << R[1] * pow(10, -3) << ") k\\Omega\n";
 }
 void Calculus::get_Q_rho()
 {
 	Q[0] = Ic0[0] / I[0];
 	Q[1] = Q[0] * multi_accuracy({ Ic0, I });
-	std::cout << "Q = (" << Q[0] << " +- " << Q[1] << ")\n";
 
 	rho[0] = R[0] / Q[0];
 	rho[1] = rho[0] * multi_accuracy({ R, Q });
-	std::cout << "rho = (" << rho[0] * pow(10, -3) << " +- " << rho[1] * pow(10, -3) << ") k\\Omega\n";
 }
 
 void Calculus::get_LC()
@@ -65,28 +73,21 @@ void Calculus::get_LC()
 
 	C[0] = 1 / (2 * pi * f0[0] * rho[0]);
 	C[1] = C[0] * multi_accuracy({ rho, f0 });
-
-	std::cout << "L = (" << L[0] * pow(10, 3) << " +- " << L[1] * pow(10, 3) << ") mH\n";
-	std::cout << "C = (" << C[0] * pow(10, 9) << " +- " << C[1] * pow(10, 9) << ") \\nuF\n";
-
-	std::cout << "\nLet's check it: f0 = \x1b[32m" << f0[0] 
-		<< "\x1b[0m = w / (2pi) = 1 / [sqrt(LC)*2pi] = \x1b[32m" 
-		<< (1/(2*pi)) / sqrt(L[0] * C[0]) << "\x1b[m\n";
 }
 void Calculus::get_vec()
 {
 	double U[2] = { 0, U_accuracy };
-	std::cout << "\nTable:\n";
 	for (int i = 0; i < table.size(); i++)
 	{
 		U[0] = table[i][1];
 		table[i].push_back(U[0] / I[0]);
 		table[i].push_back(table[i][2] * multi_accuracy({U, I}));
 	}
-	out_vec();
 }
 void Calculus::out_vec()
 {
+	std::cout << "\nTable:\n";
+	std::sort(table.begin(), table.end(), comp);
 	for (int i = 0; i < table.size(); i++)
 	{
 		std::cout << "f = " << table[i][0]
@@ -97,5 +98,18 @@ void Calculus::out_vec()
 }
 void Calculus::out()
 {
+	std::cout.precision(3);
+	std::cout << "G = (" << G[0] * pow(10, 6) << " +- " << G[1] * pow(10, 6) << ") \\nuSm\n";
+	std::cout << "R = (" << R[0] * pow(10, -3) << " +- " << R[1] * pow(10, -3) << ") k\\Omega\n";
 
+	std::cout << "Q = (" << Q[0] << " +- " << Q[1] << ")\n";
+	std::cout << "rho = (" << rho[0] * pow(10, -3) << " +- " << rho[1] * pow(10, -3) << ") k\\Omega\n";
+
+	std::cout << "L = (" << L[0] * pow(10, 3) << " +- " << L[1] * pow(10, 3) << ") mH\n";
+	std::cout << "C = (" << C[0] * pow(10, 9) << " +- " << C[1] * pow(10, 9) << ") \\nuF\n";
+
+	std::cout << "\nLet's check it: f0 = \x1b[32m" << f0[0]
+		<< "\x1b[0m = w / (2pi) = 1 / [sqrt(LC)*2pi] = \x1b[32m"
+		<< (1 / (2 * pi)) / sqrt(L[0] * C[0]) << "\x1b[m\n";
+	std::cout << "\n";
 }
